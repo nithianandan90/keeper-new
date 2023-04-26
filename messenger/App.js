5d79f72d58b41007bbbad31aeff35d529111e30f
n.maniam1990@gmail.com <n.maniam1990@gmail.com>
2023-04-27 07:24:41 +0800

    BE-3 datamodel

---
+++ messenger/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Navigator from './src/navigation';
+import {Amplify} from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import awsconfig from './src/aws-exports'; 
+ 
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Navigator />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'whitesmoke',
+    justifyContent: 'center',
+  },
+});
+
+export default withAuthenticator(App);
---
+++ messenger/src/screens/ContactsScreen.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { FlatList, View } from 'react-native';
+// import chats from '../../assets/data/chats.json';
+import ContactListItem from '../components/ContactListItem';
+import {API, graphqlOperation} from 'aws-amplify';
+import {listUsers} from '../graphql/queries';
+
+
+
+
+const ContactsScreen = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+   console.log("hsere");
+    API.graphql(graphqlOperation(listUsers)).then((result)=>{
+      console.log(result.data.listUsers.items);
+      setUsers(result.data?.listUsers?.items);
+    })     
+  },[])
+
+
+  
+
+
+  return (
+    
+    <FlatList
+      data={users}
+      renderItem={({ item }) => <ContactListItem user={item} />}
+      style={{ backgroundColor: 'white' }}
+    />
+  );
+};
+
+export default ContactsScreen;
